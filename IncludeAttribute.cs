a45efa7715b4cb460200d448e3dceb6e8ec121c4
Szymon Adach <sadach@freeportmetrics.com>
2018-11-23 10:41:53 +0100

    Fix/add comments

--- IncludeAttribute.cs
+++ IncludeAttribute.cs
@@ -3,7 +3,7 @@
 namespace ef_includer
 {
     /// <summary>
-    /// All of entity's properties marked with this attributes will be automatically included during
+    /// All of entity's properties marked with this attribute will be automatically included during
     /// EF Core eager loading if user invoked <see cref="DbSet.IncludeMarkedProperties">
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
@@ -11,7 +11,9 @@ namespace ef_includer
     {
         public bool CascadeInclude { get; private set; }
         /// <summary>
-        /// 
+        /// If this parameter is set to true, if entity's property marked with Include attribute
+        /// contains properties with this attribute (and so on), they'll be automatically included during EF Core
+        /// eager loading.
         /// </summary>
         /// <param name="cascadeInclude"></param>
         public IncludeAttribute(bool cascadeInclude = false)
